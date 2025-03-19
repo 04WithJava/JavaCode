c55644af5eb5c99d8ee204838aa441b890c24fed
iweon_zz <wldnjsdl0707@naver.com>
2025-03-19 18:29:20 +0900

    week

---
+++ week01/iweon_zz/Type.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Type {
+public static void main(String[] args) {
+    System.out.println(20);
+    System.out.println(20);
+    System.out.println(20);
+
+    }
+}
