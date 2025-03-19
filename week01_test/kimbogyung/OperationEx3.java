c4064afec08fe283a255df4a178209bf79016309
kimbokyung <bg5439780@gmail.com>
2025-03-19 20:06:51 +0900

    문제풀이

---
+++ week01_test/kimbogyung/OperationEx3.java
@@ -0,0 +1,7 @@
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 75;
+        boolean result = (score >= 80 && score >= 100);
+        System.out.println(result);
+    }
+}
