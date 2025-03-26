2f631cd147cfd344b72433a74c0872af7dc9b79a
kimbokyung <127289837+Kimbogyung@users.noreply.github.com>
2025-03-26 19:26:23 +0900

    Delete week02_test directory

--- week02_test/m6.java
+++
@@ -1,9 +0,0 @@
-public class m6 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-
-        int c = (a > b) ? a : b;
-        System.out.println("더 큰 숫자는 " + c + "입니다.");
-    }
-}
