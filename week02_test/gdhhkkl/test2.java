4ac74023f3c639c9de108f7bf1d01dae86efca3a
gdhhkkl <lee040319ww@naver.com>
2025-03-26 19:01:37 +0900

    문제코드입니다.

---
+++ week02_test/gdhhkkl/test2.java
@@ -0,0 +1,17 @@
+package org.example.week02_test;
+
+public class test2 {
+    /*거리에 따른 운송 수단 선택하기*/
+    public static void main(String[] args) {
+        int distance=8;
+        if(distance<=1){
+            System.out.println("도보를 이요하세요.");
+        }else if (distance<=5){
+            System.out.println("자전거를 이요하세요.");
+        }else if (distance<=25){
+            System.out.println("자동차를 이요하세요.");
+        } else if (distance<=150) {
+            System.out.println("바행기를 이요하세요.");
+        }
+    }
+}
