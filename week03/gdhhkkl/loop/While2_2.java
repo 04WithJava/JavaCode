a4744b632fd208f018d72e652020f8aaa20c707f
gdhhkkl <lee040319ww@naver.com>
2025-03-29 15:51:43 +0900

    반복문 코드 예제

---
+++ week03/gdhhkkl/loop/While2_2.java
@@ -0,0 +1,14 @@
+package org.example.week03;
+
+public class While2_2 {
+    public static void main(String[] args) {
+        int sum =0;
+        int i = 1;
+        sum = sum + i;
+        System.out.println("i="+i+"sum="+sum);
+        sum = sum + i;
+        System.out.println("i="+i+"sum="+sum);
+        sum = sum + i;
+        System.out.println("i="+i+"sum="+sum);
+    }
+}
