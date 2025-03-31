2cf0b9c64f036f3c1257a73eed9c2a2b1830b9f5
kimbokyung <bg5439780@gmail.com>
2025-03-31 10:56:17 +0900

    bdad 3주차 코드

---
+++ week03/kimbogyung/loop/while2_3.java
@@ -0,0 +1,29 @@
+package loop;
+
+public class while2_3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+        int endNum = 10;
+
+        while (i <= endNum) {
+            sum += i;
+            System.out.println("i=" + i + " sum= " + sum);
+            i++;
+        }
+
+        /*
+        sum += i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i++;
+
+        sum += i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i++;
+
+        sum += i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i++;
+        */
+    }
+}
