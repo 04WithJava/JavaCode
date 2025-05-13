f1d77d4e102251c82619309bf20ebd5cfd605ce5
kimbokyung <bg5439780@gmail.com>
2025-05-13 19:13:29 +0900

    배열, 메서드 코드

---
+++ week04/kimbogyung/method/MethodCasting2.java
@@ -0,0 +1,9 @@
+public class MethodCasting2 {
+    public static void main(String[] args) {
+        int number = 100;
+        printNumbr(number);
+    }
+    private static void printNumbr(double n) {
+        System.out.println("숫자 : " + n);
+    }
+}
