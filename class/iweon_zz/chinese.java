388c052580eba0fa0e91ad75294a8488fe8533b9
iweon_zz <wldnjsdl0707@naver.com>
2025-06-04 14:43:30 +0900

    어서오세요 토마토 중식당입니닷

--- class/iweon_zz/chinese.java
+++ class/iweon_zz/chinese.java
@@ -13,68 +13,12 @@ public class chinese {
         String order = scanner.nextLine();
 
         
-        class Kitchen {
-            String name = "Tomato";
-            String jjajangmyeon;
-            String jjamppong;
-            String tangsuyuk;
-
-            Kitchen() {
-                this.jjajangmyeon = "짜장면";
-                this.jjamppong = "짬뽕";
-                this.tangsuyuk = "탕수육";
-            }
-
-            void cook(String menu) {
-                System.out.println(name + ": " + menu + "을(를) 조리합니다.");
-            }
-        }
+       
 
         
-        class Guest {
-            String name = "SoBeen";
-            int money;         
-            int stomach;       
-            String menu;       
-
-            Guest(int money) {
-                this.money = money;
-                this.stomach = 0;
-            }
-
-            void eat(String menu) {
-                System.out.println(name + ": " + menu + "을(를) 맛있게 먹습니다!");
-                this.menu = menu;
-                this.stomach += 50; //한 번 먹으면 50만큼 위가 참
-                if (this.stomach > 100) this.stomach = 100;
-            }
-
-            void pay(int price) {
-                if (this.money >= price) {
-                    this.money -= price;
-                    System.out.println(name + ": " + price + "원을 결제했습니다. 남은 돈: " + this.money + "원");
-                } else {
-                    System.out.println(name + ": 돈이 부족합니다!");
-                }
-            }
-
-            void checkStomach() {
-                if (this.stomach >= 100) {
-                    System.out.println( name + ": 배가 아주 부릅니다!");
-                } else if (this.stomach >= 50) {
-                    System.out.println(name + ": 적당히 배가 찼어요.");
-                } else {
-                    System.out.println(name + ": 아직 배가 고파요.");
-                }
-            }
-
-            void sayGoodbye() {
-                System.out.println("Tomato" + ": 안녕히 가세요!");
-            }
-        }
 
 
-        Kitchen kitchen = new Kitchen();
+        kitchen kitchen = new kitchen();
         kitchen.cook(order);
 
         
